32a7c2cc22e59bba4390c961467bdf5786c2c3ca
Eoin Dowling <avieor1@gmail.com>
2023-01-25 04:19:30 +0000

    Adding ring deps

--- project.clj
+++ project.clj
@@ -3,7 +3,22 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [
+    [org.clojure/clojure "1.11.1"]
+
+    ;; jetty - HTTP server
+    [javax.servlet/servlet-api "2.5"]
+
+    ;; ring - HTTP api 
+    [ring/ring-core "1.9.6"]
+    [ring/ring-jetty-adapter "1.9.6"]
+
+    ;; reitit
+    [metosin/reitit-ring "0.5.18"]
+    [metosin/reitit-core "0.5.18"]
+
+    ;; extras
+    ]
   :main ^:skip-aot unicorn.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/unicorn/core.clj
+++ src/unicorn/core.clj
@@ -1,7 +1,19 @@
 (ns unicorn.core
-  (:gen-class))
+  (:gen-class)
+    (:require [ring.adapter.jetty :as jetty]
+            [clojure.pprint]))
+
+(defn handler [request]
+  (clojure.pprint/pprint request)
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello World"})
+
+(defn start []
+  (ring-jetty/run-jetty app {:port  3000
+                             :join? false}))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (start)
+  )
